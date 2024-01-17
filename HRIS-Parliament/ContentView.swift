2fc09c8797063f7625e54bb81977860d606d84c4
Bandit Silachai <mbandit001@yahoo.co.th>
2024-01-17 11:43:33 +0700

    Initial Commit

---
+++ HRIS-Parliament/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HRIS-Parliament
+//
+//  Created by Bandit Silachai on 17/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HRIS-Parliament/HRIS_ParliamentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HRIS_ParliamentApp.swift
+//  HRIS-Parliament
+//
+//  Created by Bandit Silachai on 17/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HRIS_ParliamentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
